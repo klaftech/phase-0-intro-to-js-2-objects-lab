22da0e08305ebe24a7ba4aa4889b2ad3a60dd082
Elchonon <ekn400@gmail.com>
2024-10-08 14:46:19 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+const employee = {
+    "name":"John Doe",
+    "streetAdress":"123 Main St",
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const newObj = {
+        ...obj,
+    }
+
+    newObj[key] = value;
+    return newObj;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    const newObj = {
+        ...obj,
+    }
+
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
+
+
+
